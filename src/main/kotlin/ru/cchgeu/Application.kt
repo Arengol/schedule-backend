4db1f38714b1e73b1726f6ef9ed46931abba4b7a
Vladimir <worobev2001@gmail.com>
2023-01-24 14:04:24 +0300

    Initial commit

---
+++ src/main/kotlin/ru/cchgeu/Application.kt
@@ -0,0 +1,14 @@
+package ru.cchgeu
+
+import io.ktor.server.application.*
+import ru.cchgeu.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.cio.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
