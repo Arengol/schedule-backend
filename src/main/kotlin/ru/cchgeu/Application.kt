cc29ab1a8c7103a12c68123534da493cbfd4a9fa
Vladimir <worobev2001@gmail.com>
2023-01-24 14:18:25 +0300

    Add database connection.

--- src/main/kotlin/ru/cchgeu/Application.kt
+++ src/main/kotlin/ru/cchgeu/Application.kt
@@ -1,6 +1,7 @@
 package ru.cchgeu
 
 import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
 import ru.cchgeu.plugins.*
 
 fun main(args: Array<String>): Unit =
@@ -8,6 +9,10 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    Database.connect(url = environment.config.property("database.url").getString(),
+        driver = "org.postgresql.Driver",
+        user = environment.config.property("database.user").getString(),
+        password = environment.config.property("database.password").getString())
     configureSecurity()
     configureSerialization()
     configureRouting()
